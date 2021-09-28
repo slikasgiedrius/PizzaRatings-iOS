87a550de3ca691cf19520e88ed4fb9178fbf1070
Giedrius Slikas <gsli@mobilepay.lt>
2021-09-28 16:32:35 +0300

    Initial commit

---
+++ PizzaRatings-iOS/PizzaRatings_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaRatings_iOSApp.swift
+//  PizzaRatings-iOS
+//
+//  Created by Giedrius on 2021-09-28.
+//
+
+import SwiftUI
+
+@main
+struct PizzaRatings_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
